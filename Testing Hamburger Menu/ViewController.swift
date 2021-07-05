78e2e505b76e9352d386518b669b3607a63ba810
MohammadAun <mohammadaun3030@gmail.com>
2021-07-05 15:34:31 -0500

    This is the hamburger menu

--- Testing Hamburger Menu/ViewController.swift
+++ Testing Hamburger Menu/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var trailing: NSLayoutConstraint!
+    @IBOutlet weak var leading: NSLayoutConstraint!
+    var menuOut = false
+    
+    @IBAction func menuTapped(_ sender: Any) {
+        
+        if menuOut == false {
+            leading.constant = 150
+            trailing.constant = -150
+            menuOut = true
+        } else  {
+            leading.constant = 0
+            trailing.constant = 0
+            menuOut = false
     }
-
-
+        
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations:{
+            self.view.layoutIfNeeded()
+        }) { (animationComplete) in
+            print("The animation is complete!")
+        }
+    }
 }
-
